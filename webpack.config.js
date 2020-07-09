f485e7c2d87f983402115432325da329244df163
krzysztof plesniak <krzysztofplesniak@interia.pl>
2020-07-09 15:13:33 +0200

    setting env=isProduction in Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,55 +3,59 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
 
-module.exports = {
-	entry: './src/index.js',
-	devtool: 'eval-cheap-module-source-map',
-	mode: 'development',
-	devServer: {
-		contentBase: path.join(__dirname, 'public'),
-		compress: true,
-		historyApiFallback: true,
-		port: 5000,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				use: ['babel-loader'],
-				include: [path.resolve(__dirname, 'src')],
-				exclude: /(node_modules)/,
-			},
-			{
-				test: /\.s?css$/,
-				use: [
-					{
-						loader: MiniCssExtractPlugin.loader,
-						options: {
-							hmr: process.env.NODE_ENV === 'development',
-							reloadAll: true,
+module.exports = env => {
+	const isProduction = env === 'production';
+
+	return {
+		entry: './src/index.js',
+		devtool: isProduction ? 'source-map' : 'eval-cheap-module-source-map',
+		mode: 'development',
+		devServer: {
+			contentBase: path.join(__dirname, 'public'),
+			compress: true,
+			historyApiFallback: true,
+			port: 5000,
+		},
+		module: {
+			rules: [
+				{
+					test: /\.(js|jsx)$/,
+					use: ['babel-loader'],
+					include: [path.resolve(__dirname, 'src')],
+					exclude: /(node_modules)/,
+				},
+				{
+					test: /\.s?css$/,
+					use: [
+						{
+							loader: MiniCssExtractPlugin.loader,
+							options: {
+								hmr: !process.isProduction === 'development',
+								reloadAll: true,
+							},
 						},
-					},
-					'css-loader',
-					'postcss-loader',
-					'sass-loader',
-				],
-			},
+						'css-loader',
+						'postcss-loader',
+						'sass-loader',
+					],
+				},
+			],
+		},
+		output: {
+			path: path.resolve(__dirname, 'public'),
+			filename: 'bundle.js',
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				title: 'Accordion component',
+				template: 'public/index.html',
+				filename: 'index.html',
+			}),
+			new MiniCssExtractPlugin({
+				filename: 'styles.css',
+			}),
 		],
-	},
-	output: {
-		path: path.resolve(__dirname, 'public'),
-		filename: 'bundle.js',
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: 'Accordion component',
-			template: 'public/index.html',
-			filename: 'index.html',
-		}),
-		new MiniCssExtractPlugin({
-			filename: 'styles.css',
-		}),
-	],
+	};
 };
 
 // const CSSExtract = new ExtractTextPlugin('styles.css');
