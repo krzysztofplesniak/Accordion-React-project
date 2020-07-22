7058db22e9e06da69b30dbd62488ac5f84be7c37
krzysztof plesniak <krzysztofplesniak@interia.pl>
2020-07-22 16:13:09 +0200

    Full custom width and height of switchOnOff component

--- src/components/SwitchOnOff.js
+++ src/components/SwitchOnOff.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Form } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
 
 const SwitchOnOff = ({ onHandlerTheme }) => {
 	const [isSwitchOn, setIsSwitchOn] = useState(false);
