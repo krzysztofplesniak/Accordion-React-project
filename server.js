cd535600b9c6f66c21675e6dbb398ea59d306f35
krzysztof plesniak <krzysztofplesniak@interia.pl>
2020-07-23 12:10:18 +0200

    step#17

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const path = require('path');
 
 const port = process.env.PORT || 3000;
+let t  = `${port}`;
 
 //Static folder
 const app = express();
 app.use(express.static(path.join(__dirname, './build')));
-app.get('/ping', (req, res) => res.send('pong'));
+app.get('/ping', (req, res) => res.send(t));
 app.get('/*', (req, res) => {
-	res.sendFile(path.join(__dirname, './build/index.html'));
 	
+	res.sendFile(path.join(__dirname, './build/index.html'));
 });
 
 app.listen(port, (req, res) => {
