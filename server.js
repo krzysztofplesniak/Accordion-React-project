5ddda0b74a9d3b79e9c1fe1a6a9a0f1e59409180
krzysztof plesniak <krzysztofplesniak@interia.pl>
2020-07-23 05:15:07 +0200

    step#5 deployment

--- server.js
+++ server.js
@@ -9,26 +9,23 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 const port1 = process.env.PORT || 3000;
-//const port2 = process.env.PORT || 3001;
+const port2 = process.env.PORT || 3001;
 const env = process.env.NODE_ENV;
 
 // JSON-SERVER-HEROKU
-// server.use(middlewares);
-// server.use(router);
-// server.listen(port2, () => {
-// 	console.log(`Server-JSON  listening at http://localhost:${port2}`);
-// });
-//app.use(express.json());
+server.use(middlewares);
+server.use(router);
+server.listen(port2, () => {
+	console.log(`Server-JSON  listening at http://localhost:${port2}`);
+});
 
 
 //Static folder
 const app = express();
-app.use(express.static(path.join(__dirname,'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', (req, res) => res.send('pong'));
 app.get('/*', (req, res) => {
 	res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port1, port1 =>
-	res.send('Apka dziala na porcie')
-);
+app.listen(port1);
